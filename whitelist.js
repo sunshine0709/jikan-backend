a98cafe3536ef0ab5e084c9e70a60112b60bf830
topdev0104 <simonweingand73@gmail.com>
2022-04-09 15:13:38 +0900

    feat:complete jikan-back-end

---
+++ whitelist.js
@@ -0,0 +1,9 @@
+const addresses = [
+  "0x17b546D3179ca33b542eD6BD9fE6656fb5D5b70E",
+  "0x9C817E9A34ED3f6da12B09B4fcB6B90da461bAc6",
+  "0xb3342232FbF3A2C435f0b40dC73C181C1fe306Db",
+];
+
+module.exports = {
+  addresses,
+};
