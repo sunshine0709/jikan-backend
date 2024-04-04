84b1a4d4e68b9a092eab49045f4d507ba444485e
sunshine0709 <webstar0104@gmail.com>
2024-04-04 03:27:51 -0400

    update: wl address lists

--- whitelist.js
+++ whitelist.js
@@ -2,6 +2,7 @@ const addresses = [
   "0x17b546D3179ca33b542eD6BD9fE6656fb5D5b70E",
   "0x9C817E9A34ED3f6da12B09B4fcB6B90da461bAc6",
   "0xb3342232FbF3A2C435f0b40dC73C181C1fe306Db",
+  "0xd73ae480556d02e03d0e13bd279410e522fcb282",
 ];
 
 module.exports = {
